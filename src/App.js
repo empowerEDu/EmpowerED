5a151568a1c1e5b7b9326dc8ed4541b62648fc79
vrindaj <vrinda.joshi@hotmail.com>
2024-06-28 16:53:56 -0400

    nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import NavBar from './NavBar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     // main app injected into html file (actually coded in JSX, translated into HTML via babel), rendered by index.js
+     <Router>
+     <div className="App">
+       <NavBar/>
+       <div className="content">
+         <Switch>
+           <Route exact path="/">
+           </Route>
+           <Route path="/">
+           </Route>
+           <Route path="/">
+           </Route>
+           <Route path="*">
+           </Route>
+         </Switch>
+       </div>
+     </div>
+     </Router>
   );
 }
 
---
+++ src/NavBar.js
@@ -0,0 +1,19 @@
+import {Link} from 'react-router-dom';
+import logo from './logo.png'; // Tell webpack this JS file uses this image
+
+
+const NavBar = () => {
+    return (  
+        <nav className="navbar">
+            <img src={logo} width="50px" alt="Logo" />
+            <div className="links">
+               <Link to="/">Home</Link>
+               <Link to="/newsletters">Newsletters</Link>
+               <Link to="/contact">Contact</Link>
+
+            </div>
+        </nav>
+    );
+}
+ 
+export default NavBar;
