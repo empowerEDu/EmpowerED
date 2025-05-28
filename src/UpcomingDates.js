81a4f016be8fb5f1723599f9dba2904343996302
adiii3692 <adinair0206@gmail.com>
2025-05-27 22:13:08 -0400

    Updated wie images and removed wie from upcoming events

--- src/UpcomingDates.js
+++ src/UpcomingDates.js
@@ -11,18 +11,16 @@ const UpcomingDates = () => {
   <ul>   
     
     <li>
-       <a href="https://docs.google.com/document/d/1YH7XN3ZtnRw1id-OayvynwTl3jeGZlZcayZ1MLLuhwU/edit?tab=t.0#heading=h.fpjtb183cwps">
       <div className="event-left-content">
         <div className="title-event">
-           <h2>Waterloo WiE Engineering Workshop</h2>
-          <div className="event-date"> May 10, 2025</div>
+           <h2>Stay Tuned!</h2>
+          <div className="event-date"></div>
         </div>
       </div>
-      <div className="event-icon"> 
-        <div className="month">May </div> 
+      {/* <div className="event-icon"> 
+        <div className="month"></div> 
         <div className="date">10</div> 
-      </div>
-       </a>
+      </div> */}
     </li>
         
   </ul>
